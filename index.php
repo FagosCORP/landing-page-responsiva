3f1aee5313e438e32b5f9cfb62b444061886738c
FagosCorp <hurleypvp1@gmail.com>
2023-04-10 22:27:41 -0300

    landing-page

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="descricao do nosso site">
+		<meta name="keywords" content="palavras,chaves">
+		<title>Projeto-01</title>
+		<link href="css/style.css" rel="stylesheet" >
+	</head>
+	<body>
+		<header>
+			<div>
+				<img src="imagens/parrot.png">
+			</div>
+			<nav class="desktop">
+				<ul class="menu">
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Servicos</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+			<nav class="mobile">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Servicos</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section class="banner-principal">
+			<div class="">
+				<form>
+					<h2>Qual seu melhor e-mail?</h2>
+					<input type="email" name="email" required>
+					<input type="submit" name="acao" value="Cadastrar!">
+				</form>
+			</div>		
+		</section>
+		<section class="descricao-autor">
+			<div class="">
+				<div class="">
+					<h2>Ryan Oliveira</h2>
+					<p> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="">
+					<img src="imagem.png">
+				</div>
+			</div>
+		</section>
+		<section class="especialidades">
+			<div class="">
+				<h2 class="title">Especialidades</h2>
+				<div class="">
+					<i class="fa fa-code" aria-hidden="true"></i>
+					<h3>JS</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="">
+					<i class="fa fa-html5" aria-hidden="true"></i>
+					<h3>HTML</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="">
+					<i class="fa fa-css3" aria-hidden="true"></i>
+					<h3>CSS3</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+		</section>
+		<section class="extras">
+			<div class="">
+				<div class="">
+					<h2 class="title">Depoimentos</h2>
+					<div class="depoimento-single">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+						<p class="nome-autor">lorem ipusm</p>	
+					</div>
+					<div class="depoimento-single">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+						<p class="nome-autor">lorem ipusm</p>	
+					</div>
+					<div class="depoimento-single">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+						<p class="nome-autor">lorem ipusm</p>	
+					</div>
+				</div>
+				<div class="">
+					<h2 class="title">Servicos</h2>
+					<div class="servicos">
+						<ul>
+							<li>lorem ipusm</li>
+							<li>lorem ipusm</li>
+							<li>lorem ipusm</li>
+							<li>lorem ipusm</li>
+							<li>lorem ipusm</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<p>Todos direitos reservados.</p>
+		</footer>	
+	</body>
+</html>
