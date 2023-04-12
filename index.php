9b44d5a8e0fd663e245ac528fd6ad76735120318
Ryan Souza Camargos de Oliveira <ryan.oliveira@HINOVA.net>
2023-04-12 20:49:34 -0300

    up date

--- index.php
+++ index.php
@@ -32,17 +32,19 @@
 			</nav>
 		</header>
 		<section class="banner-principal">
-			<div class="">
+			<div class="titulo"><h2>Qual seu melhor e-mail?</h2></div>
+			<div class="formulario">
 				<form>
-					<h2>Qual seu melhor e-mail?</h2>
 					<input type="email" name="email" required>
 					<input type="submit" name="acao" value="Cadastrar!">
 				</form>
 			</div>		
 		</section>
-		<section class="descricao-autor">
-			<div class="">
-				<div class="">
+		<section class="autor">
+				<div class="perfil-autor">
+					<img src="imagens/parrot-autor.png">
+				</div>
+				<div class="descricao-autor">
 					<h2>Ryan Oliveira</h2>
 					<p> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -58,51 +60,57 @@
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
-				<div class="">
-					<img src="imagem.png">
-				</div>
-			</div>
 		</section>
 		<section class="especialidades">
-			<div class="">
-				<h2 class="title">Especialidades</h2>
-				<div class="">
-					<i class="fa fa-code" aria-hidden="true"></i>
-					<h3>JS</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div class="titulo-especialidade">
+					<h2 class="title">Especialidades</h2>
 				</div>
-				<div class="">
-					<i class="fa fa-html5" aria-hidden="true"></i>
-					<h3>HTML</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<div class="boxes">
+					<div class="box-js">
+							<i class="fa fa-code fa-5x" aria-hidden="true"></i>
+							<h3>JS</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+					<div class="box-html">
+						<i class="fa fa-html5 fa-5x" aria-hidden="true"></i>
+						<h3>HTML</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+					<div class="box-css">
+						<i class="fa fa-css3 fa-5x" aria-hidden="true"></i>
+						<h3>CSS3</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
 				</div>
-				<div class="">
-					<i class="fa fa-css3" aria-hidden="true"></i>
-					<h3>CSS3</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
-			</div>
 		</section>
 		<section class="extras">
-			<div class="">
-				<div class="">
-					<h2 class="title">Depoimentos</h2>
-					<div class="depoimento-single">
+			<div class="depoimentos">
+				<h2 class="title">Depoimentos</h2>
+				<div class="depoimento-single">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+						<p class="nome-autor">lorem ipusm:</p>	
+				</div>	
+				<div class="depoimento-single">
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -110,35 +118,24 @@
 						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
 						<p class="nome-autor">lorem ipusm</p>	
-					</div>
-					<div class="depoimento-single">
+				</div>		
+				<div class="depoimento-single">
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
-						<p class="nome-autor">lorem ipusm</p>	
-					</div>
-					<div class="depoimento-single">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
-						<p class="nome-autor">lorem ipusm</p>	
-					</div>
+						<p class="nome-autor">lorem ipusm:</p>	
 				</div>
-				<div class="">
+			</div>
+			<div class="servico">
 					<h2 class="title">Servicos</h2>
-					<div class="servicos">
+					<div class="servicos-box">
 						<ul>
-							<li>lorem ipusm</li>
-							<li>lorem ipusm</li>
-							<li>lorem ipusm</li>
-							<li>lorem ipusm</li>
-							<li>lorem ipusm</li>
+							<li><a href="#contato">contato</a></li>
+							<li><a href="#termos">termos de servico</a></li>
+							<li><a href="#parceiros">parceiros</a></li>
 						</ul>
 					</div>
 				</div>
